1dd6af9d531c6a26824a6a6709fbd2dc2c5d634e
Asel Dzhunushalieva <aselerk@gmail.com>
2021-12-07 23:13:01 +0300

    add game gcd: mode package.json, add bin/brain/gcd.js, add src/games/gcd.js

---
+++ src/games/gcd.js
@@ -0,0 +1,35 @@
+import {
+  welcomeToGame, numOfQuestions, round, congrats,
+} from '../index.js';
+
+const getGreatestOurDivider = (smallerNumber, biggerNumber) => {
+  for (let divider = smallerNumber; divider >= 1; divider -= 1) {
+    if (biggerNumber % divider === 0 && smallerNumber % divider === 0) {
+      return divider;
+    }
+  }
+  return 1;
+};
+
+const gcdGame = () => {
+  const taskOfGame = 'Find the greatest common divisor of given numbers.';
+  const name = welcomeToGame(taskOfGame);
+  for (let counter = 1; counter <= numOfQuestions; counter += 1) {
+    const numberOne = Math.floor(Math.random() * 100);
+    const numberTwo = Math.floor(Math.random() * 100);
+    const question = `${numberOne} ${numberTwo}`;
+    const smallerNumber = (numberOne < numberTwo) ? numberOne : numberTwo;
+    const biggerNumber = (numberOne > numberTwo) ? numberOne : numberTwo;
+    const greatestOurDivider = getGreatestOurDivider(smallerNumber, biggerNumber);
+    const rightAnswer = greatestOurDivider.toString();
+    const answerIsRight = round(name, question, rightAnswer);
+    if (answerIsRight === 'wrong') {
+      return;
+    }
+    if (counter === 3) {
+      congrats(name);
+    }
+  }
+};
+
+export default gcdGame;

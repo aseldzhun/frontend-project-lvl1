ed2a7697543a925378ab910c6fae2e577906e78a
Asel Dzhunushalieva <aselerk@gmail.com>
2021-12-08 23:57:46 +0300

    add game prime: mode package.json, add bin/brain-prime.js, add src/games/prime.js

---
+++ src/games/prime.js
@@ -0,0 +1,34 @@
+import {
+  welcomeToGame, numOfQuestions, round, congrats,
+} from '../index.js';
+
+const isPrime = (number) => {
+  if (number < 4) {
+    return 'yes';
+  }
+  for (let divider = 2; divider <= (number / 2); divider += 1) {
+    if (number % divider === 0) {
+      return 'no';
+    }
+  }
+  return 'yes';
+};
+
+const primeGame = () => {
+  const taskOfGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  const name = welcomeToGame(taskOfGame);
+  for (let counter = 1; counter <= numOfQuestions; counter += 1) {
+    const number = Math.floor(Math.random() * 100);
+    const question = `${number}`;
+    const rightAnswer = isPrime(number).toString();
+    const answerIsRight = round(name, question, rightAnswer);
+    if (answerIsRight === 'wrong') {
+      return;
+    }
+    if (counter === 3) {
+      congrats(name);
+    }
+  }
+};
+
+export default primeGame;

c5c638690b544f9c81a0031a82ae3bce91ecc728
Asel Dzhunushalieva <aselerk@gmail.com>
2021-12-13 22:28:49 +0300

    create function engine in index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,23 @@
 import readlineSync from 'readline-sync';
 import greeting from './cli.js';
 
-export const welcomeToGame = (taskOfGame) => {
+export const numOfQuestions = 3;
+
+export const engine = (taskOfGame, pairsOfQuestionsAndAnswers) => {
   console.log('Welcome to the Brain Games!');
   const name = greeting();
   console.log(`Hello, ${name}!`);
   console.log(taskOfGame);
-  return name;
-};
 
-export const numOfQuestions = 3;
-
-export const round = (name, question, rightAnswer) => {
-  console.log(`Question: ${question}`);
-  const answer = readlineSync.question('Your answer: ');
-  if (answer === rightAnswer) {
+  for (let round = 0; round < pairsOfQuestionsAndAnswers.length; round += 1) {
+    const [question, answer] = pairsOfQuestionsAndAnswers[round];
+    console.log(`Question: ${question}`);
+    const receivedAnswer = readlineSync.question('Your answer: ');
+    if (receivedAnswer !== answer) {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${name}!`);
+      return;
+    }
     console.log('Correct!');
-    return 'right';
   }
-  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${name}!`);
-  return 'wrong';
-};
-export const congrats = (name) => {
   console.log(`Congratulations, ${name}!`);
 };

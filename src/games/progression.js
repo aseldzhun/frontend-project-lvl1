a3735d9e57530c808913210955080b3e67679cdc
Asel Dzhunushalieva <aselerk@gmail.com>
2021-12-13 22:46:29 +0300

    mode src/games/progression.js according new engine

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,10 +1,10 @@
 import {
-  welcomeToGame, numOfQuestions, round, congrats,
+  engine, numOfQuestions,
 } from '../index.js';
 
 const progressionGame = () => {
   const taskOfGame = 'What is the result of the expression?';
-  const name = welcomeToGame(taskOfGame);
+  const pairsOfQuestionsAndAnswers = [];
   for (let counter = 1; counter <= numOfQuestions; counter += 1) {
     const firstNumber = Math.floor(Math.random() * 100);
     const commonDifference = Math.floor(Math.random() * 10) + 1;
@@ -17,15 +17,10 @@ const progressionGame = () => {
     const hiddenNumber = progression[hiddenIndex];
     progression[hiddenIndex] = '..';
     const question = progression.join(' ');
-    const rightAnswer = hiddenNumber.toString();
-    const answerIsRight = round(name, question, rightAnswer);
-    if (answerIsRight === 'wrong') {
-      return;
-    }
-    if (counter === 3) {
-      congrats(name);
-    }
+    const answer = hiddenNumber.toString();
+    pairsOfQuestionsAndAnswers.push([question, answer]);
   }
+  engine(taskOfGame, pairsOfQuestionsAndAnswers);
 };
 
 export default progressionGame;

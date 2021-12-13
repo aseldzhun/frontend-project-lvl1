0c81cd1c31cf7f448d821f07147e9e77d5cc9661
Asel Dzhunushalieva <aselerk@gmail.com>
2021-12-13 22:29:59 +0300

    mode src/games/even.js according new engine

--- src/games/even.js
+++ src/games/even.js
@@ -1,21 +1,16 @@
 import {
-  welcomeToGame, numOfQuestions, round, congrats,
+  engine, numOfQuestions,
 } from '../index.js';
 
 const evenGame = () => {
   const taskOfGame = 'Answer "yes" if the number is even, otherwise answer "no".';
-  const name = welcomeToGame(taskOfGame);
+  const pairsOfQuestionsAndAnswers = [];
   for (let counter = 1; counter <= numOfQuestions; counter += 1) {
     const question = Math.floor(Math.random() * 100) + 1;
-    const rightAnswer = (question % 2 === 0) ? 'yes' : 'no';
-    const answerIsRight = round(name, question, rightAnswer);
-    if (answerIsRight === 'wrong') {
-      return;
-    }
-    if (counter === 3) {
-      congrats(name);
-    }
+    const answer = (question % 2 === 0) ? 'yes' : 'no';
+    pairsOfQuestionsAndAnswers.push([question, answer]);
   }
+  engine(taskOfGame, pairsOfQuestionsAndAnswers);
 };
 
 export default evenGame;

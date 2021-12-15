1ecfc2ecc772dd7acf47f0e23618f613e492c071
Asel Dzhunushalieva <aselerk@gmail.com>
2021-12-15 21:44:00 +0300

    take out function for receiving right answer from function in even.js

--- src/games/even.js
+++ src/games/even.js
@@ -4,11 +4,19 @@ import {
 
 const taskOfGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (number) => {
+  if (number % 2 === 0) {
+    return true;
+  }
+  return false;
+};
+
 const evenGame = () => {
   const pairsOfQuestionsAndAnswers = [];
   for (let counter = 1; counter <= numOfQuestions; counter += 1) {
-    const question = Math.floor(Math.random() * 100) + 1;
-    const answer = (question % 2 === 0) ? 'yes' : 'no';
+    const number = Math.floor(Math.random() * 100) + 1;
+    const question = number;
+    const answer = (isEven(number)) ? 'yes' : 'no';
     pairsOfQuestionsAndAnswers.push([question, answer]);
   }
   engine(taskOfGame, pairsOfQuestionsAndAnswers);

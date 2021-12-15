e50c2ef1bfb0aed883df1aa2d604da0906fb3186
Asel Dzhunushalieva <aselerk@gmail.com>
2021-12-15 21:02:36 +0300

    delete games.js

--- src/games.js
+++
@@ -1,9 +0,0 @@
-import greeting from './cli.js';
-
-const games = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = greeting();
-  console.log(`Hello, ${name}!`);
-};
-
-export default games;

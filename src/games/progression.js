a1c45447ff09468a18060f1ffe30f1666bb668af
Asel Dzhunushalieva <aselerk@gmail.com>
2021-12-16 23:29:33 +0300

    fix progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,25 +5,25 @@ import getRandomNumber from '../randomNumber.js';
 
 const taskOfGame = 'What is the result of the expression?';
 
-const getHiddenNumber = (progression) => {
-  const hiddenIndex = Math.floor(Math.random() * 10);
-  const hiddenNumber = progression[hiddenIndex];
-  /* eslint-disable-next-line */
-  progression[hiddenIndex] = '..';
-  return hiddenNumber.toString();
+const getProgression = () => {
+  const firstNumber = getRandomNumber(100);
+  const commonDifference = getRandomNumber(10);
+  const progressionLength = 10;
+  const progression = [firstNumber];
+  for (let index = 1; index < progressionLength; index += 1) {
+    progression.push(firstNumber + (commonDifference * index));
+  }
+  return progression;
 };
 
 const progressionGame = () => {
   const roundsData = [];
   for (let counter = 1; counter <= numOfQuestions; counter += 1) {
-    const firstNumber = getRandomNumber(100);
-    const commonDifference = getRandomNumber(10);
-    const progressionLength = 10;
-    const progression = [firstNumber];
-    for (let index = 1; index < progressionLength; index += 1) {
-      progression.push(firstNumber + (commonDifference * index));
-    }
-    const answer = getHiddenNumber(progression);
+    const progression = getProgression();
+    const hiddenIndex = getRandomNumber(10) - 1;
+    const hiddenNumber = progression[hiddenIndex];
+    progression[hiddenIndex] = '..';
+    const answer = hiddenNumber.toString();
     const question = progression.join(' ');
     roundsData.push([question, answer]);
   }

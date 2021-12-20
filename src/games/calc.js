7a482fa58c66382937749ba085c215ebcabe9803
Asel Dzhunushalieva <aselerk@gmail.com>
2021-12-20 21:04:11 +0300

    bind generation random operator through number of operators

--- src/games/calc.js
+++ src/games/calc.js
@@ -5,6 +5,7 @@ import getRandomNumber from '../randomNumber.js';
 
 const taskOfGame = 'What is the result of the expression?';
 const operators = ['+', '-', '*'];
+const operatorsNumber = operators.length;
 
 const getAnswer = (numberOne, numberTwo, operator) => {
   switch (operator) {
@@ -24,7 +25,7 @@ const evenGame = () => {
   for (let counter = 1; counter <= numOfQuestions; counter += 1) {
     const numberOne = getRandomNumber(100);
     const numberTwo = getRandomNumber(100);
-    const operator = operators[getRandomNumber(3) - 1];
+    const operator = operators[getRandomNumber(operatorsNumber) - 1];
     const question = `${numberOne} ${operator} ${numberTwo}`;
     const answer = getAnswer(numberOne, numberTwo, operator).toString();
     roundsData.push([question, answer]);
